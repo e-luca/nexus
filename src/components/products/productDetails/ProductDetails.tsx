77abe2f86a1f87fe43d621d1dbb0b20a4d31d2a6
e-luca <eldar.lucic@gmail.com>
2025-10-27 19:06:07 +0100

    Add content to product details page

--- src/components/products/productDetails/ProductDetails.tsx
+++ src/components/products/productDetails/ProductDetails.tsx
@@ -33,10 +33,110 @@ const ProductDetails = () => {
 
   return (
     <div className="product-details-container">
-      {!data ? (
+      {!product ? (
         <NoDataMessage Icon={FaBox} message="No product details available" />
       ) : (
-        <div></div>
+        <div className="product-details-content">
+          <div className="product-details-header">
+            <img
+              className="product-details-image"
+              src={product.thumbnail}
+              alt={product.description}
+            />
+            <span className="product-details-title">{product.title}</span>
+          </div>
+          <div className="product-details-body">
+            Basic Info
+            <div className="product-details-info-content">
+              <div className="product-details-info-tile">
+                <span className="product-details-info-tile-title">
+                  Category:
+                </span>{' '}
+                {product.category}
+              </div>
+              <div className="product-details-info-tile">
+                <span className="product-details-info-tile-title">Price:</span>{' '}
+                {product.price}$
+              </div>
+              <div className="product-details-info-tile">
+                <span className="product-details-info-tile-title">Rating:</span>{' '}
+                {product.rating}
+              </div>
+              <div className="product-details-info-tile">
+                <span className="product-details-info-tile-title">Brand:</span>{' '}
+                {product.brand}
+              </div>
+              <div className="product-details-info-tile">
+                <span className="product-details-info-tile-title">Tags:</span>
+                <span>
+                  {product.tags.map((tag, idx) => (
+                    <span key={idx}>#{tag} </span>
+                  ))}{' '}
+                </span>
+              </div>
+            </div>
+            Dimensions
+            <div className="product-details-info-content">
+              <div className="product-details-info-tile">
+                <span className="product-details-info-tile-title">Width:</span>{' '}
+                {product.dimensions.width} cm
+              </div>
+              <div className="product-details-info-tile">
+                <span className="product-details-info-tile-title">Height:</span>{' '}
+                {product.dimensions.height} cm
+              </div>
+              <div className="product-details-info-tile">
+                <span className="product-details-info-tile-title">Depth:</span>{' '}
+                {product.dimensions.depth} cm
+              </div>
+              <div className="product-details-info-tile">
+                <span className="product-details-info-tile-title">Weight:</span>{' '}
+                {product.weight} kg
+              </div>
+            </div>
+            Policies & Status
+            <div className="product-details-info-content">
+              <div className="product-details-info-tile">
+                <span className="product-details-info-tile-title">
+                  Warranty Information:
+                </span>{' '}
+                {product.warrantyInformation}
+              </div>
+              <div className="product-details-info-tile">
+                <span className="product-details-info-tile-title">
+                  Shipping Information:
+                </span>{' '}
+                {product.shippingInformation}
+              </div>
+              <div className="product-details-info-tile">
+                <span className="product-details-info-tile-title">
+                  Availability Status:
+                </span>{' '}
+                {product.availabilityStatus}
+              </div>
+              <div className="product-details-info-tile">
+                <span className="product-details-info-tile-title">
+                  Return Policy:
+                </span>{' '}
+                {product.returnPolicy}
+              </div>
+            </div>
+            Meta Data
+            <div className="product-details-info-content">
+              <div className="product-details-info-tile">
+                <span className="product-details-info-tile-title">Code:</span>{' '}
+                {product.meta.barcode}
+              </div>
+              <div className="product-details-info-tile">
+                <img
+                  src={product.meta.qrCode}
+                  alt="product qr code"
+                  className="product-details-qrcode"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       )}
     </div>
   );
