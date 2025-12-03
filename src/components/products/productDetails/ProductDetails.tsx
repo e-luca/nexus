884e30e4b76f63f10d2e34748cc23d8a410625ed
e-luca <eldar.lucic@gmail.com>
2025-12-04 00:41:28 +0100

    Finish recipe details page

--- src/components/products/productDetails/ProductDetails.tsx
+++ src/components/products/productDetails/ProductDetails.tsx
@@ -1,5 +1,6 @@
 import { data, useParams } from 'react-router-dom';
 import './ProductDetails.css';
+import '../../common/styles/detailsPageLayout.css';
 import { useFetch } from '../../../utils/hooks/useFetch';
 import { Product } from '../../../models/product';
 import LoadingSpinner from '../../common/loadingSpinner/LoadingSpinner';
@@ -21,59 +22,57 @@ const ProductDetails = () => {
 
   if (loading)
     return (
-      <div className="product-details-container">
+      <div className="details-page-container">
         <LoadingSpinner />
       </div>
     );
 
   if (error)
     return (
-      <div className="product-details-container">
+      <div className="details-page-container">
         <ErrorMessage />
       </div>
     );
 
   return (
-    <div className="product-details-container">
+    <div className="details-page-container">
       {!product ? (
         <NoDataMessage Icon={FaBox} message="No product details available" />
       ) : (
-        <div className="product-details-content">
-          <div className="product-details-header">
+        <div className="details-page-content">
+          <div className="details-page-header">
             <img
               className="product-details-image"
               src={product.thumbnail}
               alt={product.description}
             />
-            <span className="product-details-title">{product.title}</span>
+            <span className="details-page-title">{product.title}</span>
           </div>
-          <div className="product-details-body">
+          <div className="details-page-body">
             <fieldset className="product-details-description">
               <legend className="border-text">Description</legend>
               {product.description}
             </fieldset>
-            <fieldset className="product-details-info-content">
+            <fieldset className="details-page-info-content">
               <legend className="border-text">Info</legend>
-              <div className="product-details-info-tile">
-                <span className="product-details-info-tile-title">
-                  Category:
-                </span>{' '}
+              <div className="details-page-info-tile">
+                <span className="details-page-info-tile-title">Category:</span>{' '}
                 {product.category}
               </div>
-              <div className="product-details-info-tile">
-                <span className="product-details-info-tile-title">Price:</span>{' '}
+              <div className="details-page-info-tile">
+                <span className="details-page-info-tile-title">Price:</span>{' '}
                 {product.price}$
               </div>
-              <div className="product-details-info-tile">
-                <span className="product-details-info-tile-title">Rating:</span>{' '}
+              <div className="details-page-info-tile">
+                <span className="details-page-info-tile-title">Rating:</span>{' '}
                 {product.rating}
               </div>
-              <div className="product-details-info-tile">
-                <span className="product-details-info-tile-title">Brand:</span>{' '}
+              <div className="details-page-info-tile">
+                <span className="details-page-info-tile-title">Brand:</span>{' '}
                 {product.brand}
               </div>
-              <div className="product-details-info-tile">
-                <span className="product-details-info-tile-title">Tags:</span>
+              <div className="details-page-info-tile">
+                <span className="details-page-info-tile-title">Tags:</span>
                 <span>
                   {product.tags.map((tag, idx) => (
                     <span key={idx}>#{tag} </span>
@@ -81,60 +80,60 @@ const ProductDetails = () => {
                 </span>
               </div>
             </fieldset>
-            <fieldset className="product-details-info-content">
+            <fieldset className="details-page-info-content">
               <legend className="border-text">Dimensions</legend>
-              <div className="product-details-info-tile">
-                <span className="product-details-info-tile-title">Width:</span>{' '}
+              <div className="details-page-info-tile">
+                <span className="details-page-info-tile-title">Width:</span>{' '}
                 {product.dimensions.width} cm
               </div>
-              <div className="product-details-info-tile">
-                <span className="product-details-info-tile-title">Height:</span>{' '}
+              <div className="details-page-info-tile">
+                <span className="details-page-info-tile-title">Height:</span>{' '}
                 {product.dimensions.height} cm
               </div>
-              <div className="product-details-info-tile">
-                <span className="product-details-info-tile-title">Depth:</span>{' '}
+              <div className="details-page-info-tile">
+                <span className="details-page-info-tile-title">Depth:</span>{' '}
                 {product.dimensions.depth} cm
               </div>
-              <div className="product-details-info-tile">
-                <span className="product-details-info-tile-title">Weight:</span>{' '}
+              <div className="details-page-info-tile">
+                <span className="details-page-info-tile-title">Weight:</span>{' '}
                 {product.weight} kg
               </div>
             </fieldset>
-            <fieldset className="product-details-info-content">
+            <fieldset className="details-page-info-content">
               <legend className="border-text">Policies & Status</legend>
-              <div className="product-details-info-tile">
-                <span className="product-details-info-tile-title">
+              <div className="details-page-info-tile">
+                <span className="details-page-info-tile-title">
                   Warranty Information:
                 </span>{' '}
                 {product.warrantyInformation}
               </div>
-              <div className="product-details-info-tile">
-                <span className="product-details-info-tile-title">
+              <div className="details-page-info-tile">
+                <span className="details-page-info-tile-title">
                   Shipping Information:
                 </span>{' '}
                 {product.shippingInformation}
               </div>
-              <div className="product-details-info-tile">
-                <span className="product-details-info-tile-title">
+              <div className="details-page-info-tile">
+                <span className="details-page-info-tile-title">
                   Availability Status:
                 </span>{' '}
                 {product.availabilityStatus}
               </div>
-              <div className="product-details-info-tile">
-                <span className="product-details-info-tile-title">
+              <div className="details-page-info-tile">
+                <span className="details-page-info-tile-title">
                   Return Policy:
                 </span>{' '}
                 {product.returnPolicy}
               </div>
             </fieldset>
 
-            <fieldset className="product-details-info-content">
+            <fieldset className="details-page-info-content">
               <legend className="border-text"> Meta Data</legend>
-              <div className="product-details-info-tile">
-                <span className="product-details-info-tile-title">Code:</span>{' '}
+              <div className="details-page-info-tile">
+                <span className="details-page-info-tile-title">Code:</span>{' '}
                 {product.meta.barcode}
               </div>
-              <div className="product-details-info-tile">
+              <div className="details-page-info-tile">
                 <img
                   src={product.meta.qrCode}
                   alt="product qr code"
--- src/components/recipes/recipeDetails/RecipeDetails.tsx
+++ src/components/recipes/recipeDetails/RecipeDetails.tsx
@@ -5,6 +5,9 @@ import ErrorMessage from '../../common/errorMessage/ErrorMessage';
 import LoadingSpinner from '../../common/loadingSpinner/LoadingSpinner';
 import NoDataMessage from '../../common/noDataMessage/NoDataMessage';
 import { FaBowlRice } from 'react-icons/fa6';
+import '../../common/styles/detailsPageLayout.css';
+import './RecipeDetails.css';
+import { useMemo } from 'react';
 
 const RecipeDetails = () => {
   const { id: recipeId } = useParams<{ id: string }>();
@@ -15,32 +18,103 @@ const RecipeDetails = () => {
   } = useFetch<Recipe>(`${process.env.REACT_APP_RECIPES_API_URL!}/${recipeId}`);
   const RecipeIcon = FaBowlRice as any;
 
+  const instructions = useMemo(() => {
+    if (!recipe) return;
+
+    return recipe.instructions.map((instruction) => `${instruction} `);
+  }, [recipe]);
+
   if (error)
     return (
-      <div>
+      <div className="details-page-container">
         <ErrorMessage />
       </div>
     );
   if (loading)
     return (
-      <div>
+      <div className="details-page-container">
         <LoadingSpinner />
       </div>
     );
   return (
-    <div>
+    <div className="details-page-container">
       {!recipe ? (
         <NoDataMessage
           Icon={RecipeIcon}
           message="No details for this recipe available"
         />
       ) : (
-        <div>
-          <div>
-            <img src={recipe.image} alt={recipe.name} />
-            <span>{recipe.name}</span>
+        <div className="details-page-content">
+          <div className="details-page-header">
+            <img
+              src={recipe.image}
+              alt={recipe.name}
+              className="details-page-image recipe-details-image"
+            />
+            <span className="details-page-title">{recipe.name}</span>
           </div>
-          <div> </div>
+          <div className="details-page-body">
+            <fieldset className="instructions-ingredients">
+              <legend className="border-text">Instructions</legend>
+              {instructions}
+            </fieldset>
+            <fieldset className="instructions-ingredients">
+              <legend className="border-text">Ingredients</legend>
+              {recipe.ingredients.map((ingredient, idx) => (
+                <div key={idx}>- {ingredient}</div>
+              ))}
+            </fieldset>
+            <fieldset className="details-page-info-content">
+              <legend className="border-text">Preparation Info</legend>
+              <div className="details-page-info-tile">
+                <span className="details-page-info-tile-title">
+                  Preparation Time:
+                </span>{' '}
+                {recipe.prepTimeMinutes} min
+              </div>
+              <div className="details-page-info-tile">
+                <span className="details-page-info-tile-title">
+                  Cooking Time:
+                </span>{' '}
+                {recipe.cookTimeMinutes} min
+              </div>
+            </fieldset>
+            <fieldset className="details-page-info-content">
+              <legend className="border-text">Serving</legend>
+              <div className="details-page-info-tile">
+                <span className="details-page-info-tile-title">Servings:</span>{' '}
+                {recipe.servings}
+              </div>
+              <div className="details-page-info-tile">
+                <span className="details-page-info-tile-title">
+                  Calories per Serving:
+                </span>{' '}
+                {recipe.caloriesPerServing}
+              </div>
+            </fieldset>
+            <fieldset className="details-page-info-content">
+              <legend className="border-text">Type</legend>
+              <div className="details-page-info-tile">
+                <span className="details-page-info-tile-title">Meal type:</span>{' '}
+                {recipe.mealType}
+              </div>
+              <div className="details-page-info-tile">
+                <span className="details-page-info-tile-title">Cuisine:</span>{' '}
+                {recipe.cuisine}
+              </div>
+            </fieldset>
+            <fieldset className="details-page-info-content">
+              <legend className="border-text">Review</legend>
+              <div className="details-page-info-tile">
+                <span className="details-page-info-tile-title">Review:</span>{' '}
+                {recipe.reviewCount}
+              </div>
+              <div className="details-page-info-tile">
+                <span className="details-page-info-tile-title">Rating:</span>{' '}
+                {recipe.rating}
+              </div>
+            </fieldset>
+          </div>
         </div>
       )}
     </div>
